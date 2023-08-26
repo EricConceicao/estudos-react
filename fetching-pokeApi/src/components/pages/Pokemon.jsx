d3617f31b791388d2a3417d81ab5b14b569de48d
Eric <eric.r@aluno.ifsp.edu.br>
2023-08-26 01:36:17 -0300

    feat: added search for fetching pokémons name and type

--- fetching-pokeApi/src/components/pages/Pokemon.jsx
+++ fetching-pokeApi/src/components/pages/Pokemon.jsx
@@ -1,6 +1,44 @@
+// Library imports
+import {useState} from 'react';
+
+import './Pokemon.css';
+
 function Pokemon() {
+
+    const [name, setName] = useState('');
+    const [data, setData] = useState({});
+
+    async function fetchPokemon(e) {
+        e.preventDefault();
+
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`);
+        const newData = await res.json()
+        setData(newData); 
+
+        console.log(newData);
+    }
+
     return (
-        <h1 className="display-1">Digite o nome do pokémon que quer pesquisar!</h1>
+        <div className="Pokemon container">
+            <h1 className="display-1">Digite o nome do pokémon que quer pesquisar!</h1>
+
+            <form onSubmit={fetchPokemon}>
+                <input type="text" onChange={(e) => setName(e.target.value)} placeholder="ditto..."/>
+                <button className="btn-primary" type="submit">Pesquisar</button>
+            </form>
+
+            
+                <figure className="border p-2 d-inline-block ">
+                    {data && <img clasName="image-thumbnail" src={data?.sprites?.front_default} alt={`Image of ${data?.name}`} />}
+                    <figcaption className="display-6">{data?.name}</figcaption>
+                </figure>
+            
+            <h2>
+                {data?.types?.map(poke => {
+                    return poke.type.name + " ";
+                })}
+            </h2>
+        </div>
     );
 }
 
